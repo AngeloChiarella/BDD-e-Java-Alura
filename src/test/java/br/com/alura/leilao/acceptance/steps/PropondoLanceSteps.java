ebb3abcc8a8cdbf1e95804a5641cfdaad46dce5e
AngeloChiarella <chiarella.gaetano@gmail.com>
2022-08-23 15:59:42 -0300

    Solução com Exemplo

--- src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
+++ src/test/java/br/com/alura/leilao/acceptance/steps/PropondoLanceSteps.java
@@ -76,14 +76,15 @@ public class PropondoLanceSteps {
 		Assert.assertEquals(this.lista.get(1).getValor(), leilao.getLances().get(1).getValor());
 	}
 
-	@Dado("um lance invalido de {double} reais")
-	public void um_lance_de_reais(Double valor) {
-		System.out.println(valor);
+	@Dado("um lance invalido de {double} reais e do usuario {string}")
+	public void um_lance_de_reais(Double valor, String nomeUsuario) {
+		System.out.println(nomeUsuario);
+		this.lance = new Lance(new BigDecimal(valor));
 	}
 
 	@Entao("o lance nao eh aceito")
 	public void o_lance_nao_eh_aceito() {
-		
+		Assert.assertEquals(0, leilao.getLances().size());
 	}
 
 }
